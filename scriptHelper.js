17c10f2e284ad9f17e48f572ae65a89f3c4d057c
Teli <7telita@gmail.com>
2023-02-28 15:25:48 -0600

    fixed equal

--- scriptHelper.js
+++ scriptHelper.js
@@ -45,7 +45,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         alert("Make sure to enter valid information for each field");
     } else {
 
-        if ((fuelLevel) < 10000 || (cargoLevel) > 10000) {
+        if ((fuelLevel) <= 10000 || (cargoLevel) > 10000) {
         list.style.visibility = "visible";
         launchStatus.style.color = "rgb(199, 37, 78)";
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
